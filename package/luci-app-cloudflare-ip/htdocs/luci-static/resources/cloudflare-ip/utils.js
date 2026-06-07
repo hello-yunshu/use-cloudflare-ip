65f73fb030bef51b41d075480e20b5ca1a16bb30
云云朮 <louisowen@me.com>
2026-06-07 21:54:17 +0800

    chore: bump version to 1.6.5

--- package/luci-app-cloudflare-ip/htdocs/luci-static/resources/cloudflare-ip/utils.js
+++ package/luci-app-cloudflare-ip/htdocs/luci-static/resources/cloudflare-ip/utils.js
@@ -3,7 +3,7 @@
 'require baseclass';
 
 var FOOTER_VERSION = '@PKG_VERSION@';
-var FOOTER_VERSION_FALLBACK = '1.6.3';
+var FOOTER_VERSION_FALLBACK = '1.6.5';
 
 function safeApply() {
 	return uci.apply().catch(function(e) {
